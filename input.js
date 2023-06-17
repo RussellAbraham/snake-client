a6a93e97e30c1361bbecc08af9d786e9650b348b
RussellAbraham <russell_a@live.com>
2023-06-17 13:30:44 -0700

    update input to handle w-a-s-d, store active TCP connection

--- input.js
+++ input.js
@@ -1,3 +1,5 @@
+// Stores the active TCP connection object.
+let connection;
 
 // setup interface to handle user input from stdin
 const setupInput = function () {
@@ -16,7 +18,16 @@ const handleUserInput = function (event) {
     process.exit();
   } 
   if(event === 'w'){
-    console.log('w was pressed')
+    console.log('Move: up');
+  }
+  if(event === 'a'){
+    console.log('Move: left');
+  }
+  if(event === 's'){
+    console.log('Move: down');
+  }
+  if(event === 'd'){
+    console.log('Move: right');
   }
 };
 
