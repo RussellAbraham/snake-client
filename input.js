49d6813c1517c28b6ed169e25b5065f2a5c9b0de
RussellAbraham <russell_a@live.com>
2023-06-17 13:46:14 -0700

    Add message sending key handlers

--- input.js
+++ input.js
@@ -15,38 +15,44 @@ const setupInput = function (conn) {
 };
 
 const handleUserInput = function (event) {
-  if(event === '\u0003'){
-    process.exit();
-  } 
   let currentDirection = 'start';
-  if(event === 'w'){
+  if (event === '\u0003') {
+    process.exit();
+  }
+  else if (event === 'w') {
     console.log('Move: up');
     currentDirection === 'up';
-    if(currentDirection !== 'down'){
+    if (currentDirection !== 'down') {
       connection.write('Move: up');
     }
   }
-  if(event === 'a'){
+  else if (event === 'a') {
     console.log('Move: left');
     currentDirection = 'left';
-    if(currentDirection !== 'right'){
+    if (currentDirection !== 'right') {
       connection.write('Move: left');
     }
   }
-  if(event === 's'){
+  else if (event === 's') {
     console.log('Move: down');
     currentDirection = 'down';
-    if(currentDirection !== 'up'){
+    if (currentDirection !== 'up') {
       connection.write('Move: down');
     }
   }
-  if(event === 'd'){
+  else if (event === 'd') {
     console.log('Move: right');
     currentDirection = 'right';
-    if(currentDirection !== 'left'){
+    if (currentDirection !== 'left') {
       connection.write('Move: right');
     }
   }
+  else if (event === 'h'){
+    connection.write('Say: Hello World!');
+  }
+  else if (event === 'g'){
+    connection.write('Say: Goodbye World!');
+  }  
 };
 
 module.exports = {
