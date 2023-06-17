17851477f14dbf6f7fa548e277aa7b9ed237d65a
RussellAbraham <russell_a@live.com>
2023-06-17 13:25:16 -0700

    create input module to handle stdin

---
+++ input.js
@@ -0,0 +1,25 @@
+
+// setup interface to handle user input from stdin
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  stdin.on("data", handleUserInput);
+
+  return stdin;
+};
+
+const handleUserInput = function (event) {
+  if(event === '\u0003'){
+    process.exit();
+  } 
+  if(event === 'w'){
+    console.log('w was pressed')
+  }
+};
+
+module.exports = {
+  setupInput
+};
--- play.js
+++ play.js
@@ -1,26 +1,6 @@
 const { connect } = require('./client');
+const setupInput = require('./input');
 
 console.log('connecting...');
-
-// setup interface to handle user input from stdin
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-
-  stdin.on("data", handleUserInput);
-
-  return stdin;
-};
-
-const handleUserInput = function (event) {
-  if(event === '\u0003'){
-    process.exit();
-  } 
-  if(event === 'w'){
-    console.log('w was pressed')
-  }
-};
 
 setupInput(connect());
